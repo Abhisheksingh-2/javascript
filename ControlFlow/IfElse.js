f78a98c33b282b8362f040b153cba9742b22d953
Abhisheksingh-2 <Abhisheks8319@gmail.com>
2024-01-13 11:59:49 +0530

    controlFlowMethod

---
+++ ControlFlow/IfElse.js
@@ -0,0 +1,12 @@
+// if statement: when condition is true then if statement work and enter the inside the if statement and run the statements.
+// else statement : It is always used with if statement and if statement is wrong then run else statement.
+
+let val=10;
+if(val<=10){  // if and else statement work in scope method.
+    console.log("TRUE Statement");
+}
+else{
+    console.log("FALSE Statement");
+}
+// var: It is used to store the variable value and it is not a block scope .
+// let and const  is used block scope is more efficient compare to var keyword to store the variable value.
