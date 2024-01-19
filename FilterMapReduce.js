8d6119c6c8c130eb0bd5798afc74aabc5ee5d44a
Abhisheksingh-2 <Abhisheks8319@gmail.com>
2024-01-19 11:59:32 +0530

    addFilterConcept

--- FilterMapReduce.js
+++ FilterMapReduce.js
@@ -36,3 +36,65 @@ array.push(num);
     }
 });
 console.log(array);
+
+// We create a Student Database
+const StudentDatabase=[{
+    Student_ID:'STU001',
+    Student_Name:'ABHISHEK SINGH',
+    Student_Age:23,
+    Student_Course:'B.tech(CSE)',
+    Annual_Fees:45000   
+},
+{
+    Student_ID:'STU002',
+    Student_Name:'KANTI SINGH',
+    Student_Age:23,
+    Student_Course:'B.tech(IT)',
+    Annual_Fees:40000   
+},
+{
+    Student_ID:'STU003',
+    Student_Name:'YAJRAJ LODHI',
+    Student_Age:21,
+    Student_Course:'B.tech(IT)',
+    Annual_Fees:40000   
+},
+{
+    Student_ID:'STU004',
+    Student_Name:'PRASHANT TIWARI',
+    Student_Age:24,
+    Student_Course:'B.COM(HONS)',
+    Annual_Fees:35000   
+},
+{
+    Student_ID:'STU005',
+    Student_Name:'ADARSH SINGH',
+    Student_Age:22,
+    Student_Course:'MBA',
+    Annual_Fees:65000   
+},
+{
+    Student_ID:'STU006',
+    Student_Name:'SHARDA GUPTA',
+    Student_Age:23,
+    Student_Course:'B.COM',
+    Annual_Fees:30000   
+},
+{
+    Student_ID:'STU007',
+    Student_Name:'ANIKET GUPTA',
+    Student_Age:21,
+    Student_Course:'BSC(CS)',
+    Annual_Fees:40000   
+}
+];
+let stud=StudentDatabase.filter( (stu)=> stu.Student_Name==='ABHISHEK SINGH');
+console.log(stud);
+stud=StudentDatabase.filter( (stu)=>{
+    return stu.Annual_Fees>=40000;
+})
+console.log(stud);
+stud=StudentDatabase.filter( (stu)=>{
+    return stu.Annual_Fees>=40000 && stu.Student_Course==='B.tech(IT)';
+})
+console.log(stud);
