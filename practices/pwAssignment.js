e78e4e5983f93eb04f93a95753aed94944c6d6b5
Abhisheksingh-2 <Abhisheks8319@gmail.com>
2024-02-13 19:46:37 +0530

    Assignment

--- practices/pwAssignment.js
+++ practices/pwAssignment.js
@@ -16,3 +16,40 @@ console.log(min_arr);
 arr=arr_student.length-1;
 max_arr=arr_student[arr];
 console.log(max_arr);
+const median=arr_student[(arr-1)/2];
+console.log(median);
+console.log(arr_student);
+const average=arr_student.map(()=>{
+    sum=0;
+    for(i=0;i<arr_student.length;i++){
+sum=sum+arr_student[i];
+    }
+    return sum/(arr_student.length);
+})
+console.log(average);
+const range=max_arr-min_arr;
+console.log(range);
+
+//3.
+const contact_information=[{
+    name:'ABHISHEK',
+    age:22,
+    email:'abhisheksinghthakur728@gmail.com',
+    location:'satna'
+}];
+contact_information.map((e)=>{
+    console.log(e.name);
+});
+
+
+const person= {
+    name:'Abhishek',
+    age:22
+};
+    person.introduce = function (){
+console.log(`Hello,I'm ${this.name} and I'm ${this.age} years old.`);
+    }
+
+
+
+console.log(person.introduce);
