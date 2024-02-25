3781cd87d7e07b84c397281cb895247737a4d052
Abhisheksingh-2 <Abhisheks8319@gmail.com>
2024-02-25 20:20:46 +0530

    Basic Promises

--- API/promises.js
+++ API/promises.js
@@ -11,3 +11,24 @@ promiseOne.then(function(){
     console.log('Promise Consumed');
 })
 
+new Promise(function(resolve,reject){
+    setTimeout(function(){
+        console.log('Async task 2');
+        resolve()
+    },1000);
+    
+    }).then(function(){
+        console.log('Async 2 Resolved');
+    })
+    
+    
+    const promiseThree= new Promise(function(resolve,reject){
+    setTimeout(function(){
+        resolve({username:'ABHISHEK',Email:'Abhisheksinghthakur728@gmail.com'});
+    
+    },1000)
+    })
+    
+    promiseThree.then(function(user){
+        console.log(user);
+    })
