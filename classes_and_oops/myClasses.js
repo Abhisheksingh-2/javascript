4a716d859b03fa2ab4c678f997636dd74df09713
Abhisheksingh-2 <Abhisheks8319@gmail.com>
2024-03-02 15:37:41 +0530

    classes Basic

--- classes_and_oops/myClasses.js
+++ classes_and_oops/myClasses.js
@@ -20,3 +20,22 @@ console.log(usercall.changeUsername());
 console.log(usercall);
 
 
+//...............BEhind the scene......................
+
+function User(username,email,password){
+    this.username=username;
+    this.email=email;
+    this.password=password;
+}
+
+User.prototype.encryptedPassword=function(){
+    return `${this.password}abc`;
+}
+
+User.prototype.changePassword=function(){
+    return `${this.username.toLowerCase()}`;
+}
+
+const Usercall=new User('ABHISHEK SINGH','AS123@gmail.com','12345');
+console.log(Usercall.encryptedPassword());
+console.log(Usercall.changePassword());
